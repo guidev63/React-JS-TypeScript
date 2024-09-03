229f1124998cc0edf20256ed88318dd73198dd17
Guilherme Augusto <guilhermedev23@gmail.com>
2024-09-03 15:04:31 -0300

    ok

---
+++ vite-backend/src/routes.ts
@@ -0,0 +1,1 @@
+import { FastifyInstance, FastifyPluginOptions, FastifyRequest } from 'fastify';
---
+++ vite-backend/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const app = fastify({ logger: true });
+
+const start = async () => {
+  try {
+    await app.listen({ port: 3333 });
+  } catch (err) {
+    process.exit(1);
+  }
+};
+
+start();
